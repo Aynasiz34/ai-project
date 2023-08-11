fd4ad8f9178edd6cadd3cc43611adbec6d9a51a4
Aynasiz34 <gomserim@gmail.com>
2023-08-11 15:40:59 +0300

    navbar created

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Menu } from "lucide-react";
+
+export default function Home() {
+  return (
+    <div className="text-green-500 text-3xl">
+      <Menu />
+      Hello AI Companion
+    </div>
+  );
+}
