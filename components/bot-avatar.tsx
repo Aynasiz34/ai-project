742021055e826d458882366375cb101fec8ca78e
Aynasiz34 <gomserim@gmail.com>
2023-08-15 16:25:30 +0300

    chat-header created

---
+++ components/bot-avatar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Avatar, AvatarImage } from "./ui/avatar";
+
+interface BotAvatarProps {
+  src: string;
+}
+
+const BotAvatar = ({ src }: BotAvatarProps) => {
+  return (
+    <Avatar className="h-12 w-12">
+      <AvatarImage src={src} />
+    </Avatar>
+  );
+};
+
+export default BotAvatar;
